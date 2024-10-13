97218be5a3a0468168f876ed09c9515d0e0525e9
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-10-13 14:17:50 +0700

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const UnsplashPhotoGrid = () => {
+    const [photo, setPhoto] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+    }, [])
+
+    const fetchPhotos = () => {
+        setLoading(true);
+        try {
+            // todo fetch photos from unsplashAPI
+
+            const mockData = Array(12).fill().map((_, index) => ({
+                id: index,
+                urls: {thumb: `/api/placeholder/400/${300 + index * 10}`},
+                user: {
+                    name: `Photographer ${index + 1}`
+                }
+            }))
+
+            setPhoto(mockData);
+        } catch (e) {
+            setError("Failed to fetch photos");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    // if (loading) {
+    //     return <div className="text-center">Loading...</div>
+    // }
+    //
+    // if (error) {
+    //     return <div className="text-center text-red-500">Error: {error}</div>;
+    // }
+
+
+    return (
+        <div className="container">
+            <h1 className="text-3xl font-bold underline">Unsplash API Photo</h1>
+            <div>
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default UnsplashPhotoGrid;
